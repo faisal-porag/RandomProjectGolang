c67cbba4034ba00642daf4b09a1e3b65d057cc44
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-03-31 09:52:45 +0600

    scientific notation (mantissa/exponent)

--- StringFormatter/stringformatter.go
+++ StringFormatter/stringformatter.go
@@ -1,10 +1,44 @@
 
 package StringFormatter
 
+import (
+    "fmt"
+)
+
 func ShowTransactionId(trnxID string) string {
 	if trnxID != "" {
 		return trnxID[0:10] //RETURN 1ST 10 CHARACTER
 	}
 
 	return ""
+}
+
+
+func ScientificNotation() {
+
+    val := 1273.78888769000
+
+    fmt.Printf("%f\n", val)
+    fmt.Printf("%e\n", val)
+    fmt.Printf("%g\n", val)
+    fmt.Printf("%E\n", val)
+    fmt.Printf("%G\n", val)
+
+    fmt.Println("-------------------------")
+
+    fmt.Printf("%.10f\n", val)
+    fmt.Printf("%.10e\n", val)
+    fmt.Printf("%.10g\n", val)
+    fmt.Printf("%.10E\n", val)
+    fmt.Printf("%.10G\n", val)
+
+    fmt.Println("-------------------------")
+
+    val2 := 66_000_000_000.1200
+
+    fmt.Printf("%f\n", val2)
+    fmt.Printf("%e\n", val2)
+    fmt.Printf("%g\n", val2)
+    fmt.Printf("%E\n", val2)
+    fmt.Printf("%G\n", val2)
 }
