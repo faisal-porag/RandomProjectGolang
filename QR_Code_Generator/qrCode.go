69eea835801a818965ce6255cd6fb932bb29a8de
porag_faisal123@hotmail.com <porag_faisal123@hotmail.com>
2022-03-31 23:13:13 +0600

    QR code generate with img

---
+++ QR_Code_Generator/qrCode.go
@@ -0,0 +1,33 @@
+package QR_Code_Generator
+
+import (
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+	"image/png"
+	"log"
+	"os"
+)
+
+func QRCOdeGeneratorIMG() {
+	// Create the barcode
+	qrCode, _ := qr.Encode("FAISAL PORAG", qr.M, qr.Auto)
+
+	// Scale the barcode to 200x200 pixels
+	qrCode, _ = barcode.Scale(qrCode, 200, 200)
+
+	// create the output file
+	file, _ := os.Create("qrcode_dd.png")
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(file)
+
+	// encode the barcode as png
+	err := png.Encode(file, qrCode)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
