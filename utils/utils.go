a015cc753826357c0d83d3a95615ac53484a1117
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-10 09:10:11 +0600

    binarySearch example

--- utils/utils.go
+++ utils/utils.go
@@ -144,3 +144,28 @@ func DateTimeFormatExample() {
     	fmt.Println("Short Hour Minute Second: ", currentTime.Format("2006-01-02 3:4:5 pm")) 
 }
 
+
+//TODO EXAMPLE (binarySearch)
+//items := []int{1,2, 9, 20, 31, 45, 63, 70, 100}
+//fmt.Println(binarySearch(63, items))
+func binarySearch(needle int, haystack []int) bool {
+
+	low := 0
+	high := len(haystack) - 1
+
+	for low <= high{
+		median := (low + high) / 2
+
+		if haystack[median] < needle {
+			low = median + 1
+		}else{
+			high = median - 1
+		}
+	}
+
+	if low == len(haystack) || haystack[low] != needle {
+		return false
+	}
+
+	return true
+}
