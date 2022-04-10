63353281128d9c3df5a0be5c5839087a25158f77
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-10 08:54:29 +0600

    Convert given string to snake_case

--- utils/utils.go
+++ utils/utils.go
@@ -88,4 +88,17 @@ func ConvertSliceToString(input []int) string {
 //Slice converted string :  10,20,30,40
 
 
+//Convert given string to snake_case
+func ConvertToSnakeCase(input string) string {
+  var matchChars = regexp.MustCompile("(.)([A-Z][a-z]+)")
+  var matchAlpha = regexp.MustCompile("([a-z0-9])([A-Z])")
+  
+  snake := matchChars.ReplaceAllString(input, "${1}_${2}")
+  snake = matchAlpha.ReplaceAllString(snake, "${1}_${2}")
+  return strings.ToLower(snake)
+}
+//input: ILikeProgrammingINGo123
+//output:
+//String in snake case :  i_like_programming_in_go123
+
 
