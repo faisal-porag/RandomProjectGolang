8e124913869574df3a2f135a183002d1511999a7
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-12 08:55:12 +0600

    trim example added ....

--- utils/utils.go
+++ utils/utils.go
@@ -257,3 +257,25 @@ func SplitingExample() {
     fmt.Println("Result 4: ", res4)
  
 }
+
+func TrimingExample() {
+    // Creating and initializing string
+    // Using shorthand declaration
+    str1 := "!!Welcome to GeeksforGeeks !!"
+    str2 := "@@This is the tutorial of Golang$$"
+  
+    // Displaying strings
+    fmt.Println("Strings before trimming:")
+    fmt.Println("String 1: ", str1)
+    fmt.Println("String 2:", str2)
+  
+    // Trimming the given strings
+    // Using Trim() function
+    res1 := strings.Trim(str1, "!")
+    res2 := strings.Trim(str2, "@$")
+  
+    // Displaying the results
+    fmt.Println("\nStrings after trimming:")
+    fmt.Println("Result 1: ", res1)
+    fmt.Println("Result 2:", res2)
+}
