6e87ccfe2441e95046f03a4c9d0238a91eaaca4f
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-12 09:17:55 +0600

    multiple ways to loop an array

--- utils/utils.go
+++ utils/utils.go
@@ -314,3 +314,30 @@ func ValidDateChecker() {
 	fmt.Printf("Date: %v :%v\n", str7, re.MatchString(str7))
 	fmt.Printf("Date: %v :%v\n", str8, re.MatchString(str8))
 }
+
+func LoopsInArray() {
+	intArray := [5]int{10, 20, 30, 40, 50}
+
+	fmt.Println("\n---------------Example 1--------------------\n")
+	for i := 0; i < len(intArray); i++ {
+		fmt.Println(intArray[i])
+	}
+
+	fmt.Println("\n---------------Example 2--------------------\n")
+	for index, element := range intArray {
+		fmt.Println(index, "=>", element)
+
+	}
+
+	fmt.Println("\n---------------Example 3--------------------\n")
+	for _, value := range intArray {
+		fmt.Println(value)
+	}
+
+	j := 0
+	fmt.Println("\n---------------Example 4--------------------\n")
+	for range intArray {
+		fmt.Println(intArray[j])
+		j++
+	}
+}
