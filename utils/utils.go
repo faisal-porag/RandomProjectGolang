dac18dcc8bb1b31dc859d29f6f8d4a092d5cf085
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-12 08:52:40 +0600

    split example added

--- utils/utils.go
+++ utils/utils.go
@@ -231,3 +231,29 @@ func GetAllPossibleSubstringCombinationOfAString() {
 		//break
 	}
 }
+
+func SplitingExample() {
+    // Creating and initializing the strings
+    str1 := "Welcome, to the, online portal, of GeeksforGeeks"
+    str2 := "My dog name is Dollar"
+    str3 := "I like to play Ludo"
+ 
+    // Displaying strings
+    fmt.Println("String 1: ", str1)
+    fmt.Println("String 2: ", str2)
+    fmt.Println("String 3: ", str3)
+ 
+    // Splitting the given strings
+    // Using SplitAfterN() function
+    res1 := strings.SplitAfterN(str1, ",", 2)
+    res2 := strings.SplitAfterN(str2, "", 4)
+    res3 := strings.SplitAfterN(str3, "!", 1)
+    res4 := strings.SplitAfterN("", "GeeksforGeeks, geeks", 3)
+ 
+    // Displaying the result
+    fmt.Println("\nResult 1: ", res1)
+    fmt.Println("Result 2: ", res2)
+    fmt.Println("Result 3: ", res3)
+    fmt.Println("Result 4: ", res4)
+ 
+}
