39b7e088c131e9a25a97c22d031220cd217dfd5a
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-17 09:05:56 +0600

    pg connection setup

---
+++ database/pg_connection.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	// library for conenct postgresql
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+var (
+	// DB variable for connection DB postgresql
+	DB *sql.DB
+)
+
+// Connect function for checking connection to postgresql
+func PGConnect() error {
+	host := viper.GetString("configDB.host")
+	port := viper.GetString("configDB.port")
+	user := viper.GetString("configDB.user")
+	password := viper.GetString("configDB.password")
+	dbname := viper.GetString("configDB.dbname")
+
+	// psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
+	// log.Println(psqlInfo)
+	result, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		// utils.FailError(err, "Check your config file, Database not connect")
+		return err
+	}
+	// defer result.Close()
+	err = result.Ping()
+	if err != nil {
+		log.Println("Error DB Ping : ", err)
+		return err
+	}
+
+	DB = result
+	return nil
+}
