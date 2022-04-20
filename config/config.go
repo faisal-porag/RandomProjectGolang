f5030af88b6c1115b0d582b948d152f550fbf58e
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-04-20 15:20:47 +0600

    command added (How to set env data into the config file)

--- config/config.go
+++ config/config.go
@@ -14,6 +14,8 @@ type config struct {
 
 var Config config
 
+// env $(cat .env | xargs) go run main.go -> command added (How to set env data into the config file)
+
 func init() {
 	if err := env.Parse(&Config); err != nil {
 		log.Printf("Config loading failed: %+v\n", err)
