cdbb5a1c167fde2a18fdbfda8b9bc5d70d43e471
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-03 11:19:18 +0600

    Simple api expose

---
+++ api/simple_api.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func SimpleAPIExample() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8086", router))
+}
