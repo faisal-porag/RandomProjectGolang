d71f125ce173116723d9d4b5eb7a970d0f1a12a3
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-04 08:09:14 +0600

    Update port number

--- api/simple_api_v1.go
+++ api/simple_api_v1.go
@@ -13,7 +13,7 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
     http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func SimpleAPIV1Example() {
