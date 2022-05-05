35a9ed34e0d42f1baff3a8fa5bd3adfb7b439b6f
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-05 10:01:06 +0600

    Create dummy event api expose

--- api/simple_api.go
+++ api/simple_api.go
@@ -10,12 +10,44 @@ import (
 
 //Simple API creation example
 
+//MAKE dummy daabse 
+
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+type allEvents []event
+
+var events = allEvents{
+	{
+		ID:          "1",
+		Title:       "Introduction to Golang",
+		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+	},
+}
+
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
 }
 
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+	
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
 func SimpleAPIExample() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
-	log.Fatal(http.ListenAndServe(":8086", router))
+	router.HandleFunc("/event", createEvent).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8084", router))
 }
