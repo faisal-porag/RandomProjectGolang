a2e291391be7b1880e6d27f9848cab75003564b3
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-08 09:54:43 +0600

    event delete endpoint added

--- api/simple_api.go
+++ api/simple_api.go
@@ -86,6 +86,18 @@ func updateEvent(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Delete event endpoint
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "The event with ID %v has been deleted successfully", eventID)
+		}
+	}
+}
+
 func SimpleAPIExample() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
