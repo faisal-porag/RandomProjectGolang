ffe5fdea5a8dee7631b8061d779b470c139d3fef
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-09 09:44:54 +0600

    update web scraping version 1

--- web_page_scraping/web_page_scraping_v1.go
+++ web_page_scraping/web_page_scraping_v1.go
@@ -1,1 +1,46 @@
 
+package web_page_scraping
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"net/http"
+)
+
+//NOTE: GET the goquery command
+// go get -u github.com/PuerkitoBio/goquery
+
+func WebPageScrapingVersion1() {
+
+	webPage := "http://webcode.me"
+	resp, err := http.Get(webPage)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		errLog := Body.Close()
+		if errLog != nil {
+			log.Fatal(errLog)
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := doc.Find("title").Text()
+	fmt.Println(title)
+
+	title1 := doc.Find("p").Text()
+	fmt.Println(title1)
+}
