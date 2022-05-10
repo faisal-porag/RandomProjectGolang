8cd55809c134ea27bf29fd29ac97745de9aa0953
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-10 09:47:41 +0600

    update version 4

--- web_page_scraping/web_page_scraping_v4.go
+++ web_page_scraping/web_page_scraping_v4.go
@@ -1,1 +1,50 @@
 
+package web_page_scraping
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"net/http"
+)
+/*
+func WebPageScrapingVersion4() {
+	// the above code
+
+	response, err := http.Get("http://journaldev.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		errLog := Body.Close()
+		if errLog != nil {
+			log.Fatal(errLog)
+		}
+	}(response.Body)
+
+	//n, err := io.Copy(os.Stdout, response.Body)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println("Number of bytes copied to STDOUT:", n)
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".articles.article--list.gy--lg h4.article__title").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		fmt.Printf("next")
+		txt := s.Text()
+		fmt.Printf("Article %d: %s\n\n", i, txt)
+
+		//txt1 := doc.Find(".articles.article--list.gy--lg p").Text()
+		//fmt.Printf("%s\n", txt1)
+
+	})
+
+}
+*/
