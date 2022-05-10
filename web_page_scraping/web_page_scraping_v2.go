86164a9ba45cb8bbbc0745a89f2c3366aef1ffe4
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-10 09:42:34 +0600

    web page scraping version 2

--- web_page_scraping/web_page_scraping_v2.go
+++ web_page_scraping/web_page_scraping_v2.go
@@ -1,1 +1,30 @@
+package web_page_scraping
 
+import (
+	// import standard libraries
+	"fmt"
+	"log"
+
+	// import third party libraries
+	"github.com/PuerkitoBio/goquery"
+)
+
+func PostScrapeData() {
+	doc, err := goquery.NewDocument("http://jonathanmh.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// use CSS selector found with the browser inspector
+	// for each, use index and item
+	doc.Find("#main article .entry-title").Each(func(index int, item *goquery.Selection) {
+		title := item.Text()
+		linkTag := item.Find("a")
+		link, _ := linkTag.Attr("href")
+		fmt.Printf("Post #%d: %s - %s\n", index, title, link)
+	})
+}
+
+func WebPageScrapingVersion2() {
+	PostScrapeData()
+}
