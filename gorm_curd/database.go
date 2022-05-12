32f14de65b46f82aa07a4cfd5417630f42615b72
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-13 01:15:46 +0600

    database connection file added

---
+++ gorm_curd/database.go
@@ -0,0 +1,22 @@
+package gorm_curd
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+var err error
+
+func InitialMigration() {
+	DB, err = gorm.Open(mysql.Open(ConnectionString), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Can't connect to DB!")
+	}
+
+	DB.AutoMigrate(&User{})
+}
