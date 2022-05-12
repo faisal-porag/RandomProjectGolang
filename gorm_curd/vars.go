c9ea116681d92696416d683a44acfaf55c9a449e
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-13 01:20:21 +0600

    Cargar del archivo llamado ".env"

---
+++ gorm_curd/vars.go
@@ -0,0 +1,20 @@
+package gorm_curd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var _ = godotenv.Load(".env") // Cargar del archivo llamado ".env"
+var (
+	ConnectionString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("user"),
+		os.Getenv("pass"),
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("db_name"))
+)
+
+const AllowedCORSDomain = "http://localhost"
