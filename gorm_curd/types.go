a66ff6033ff963a9feaa1a83949ae831eef7e0ad
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-05-13 01:18:31 +0600

    User struct added

---
+++ gorm_curd/types.go
@@ -0,0 +1,15 @@
+package gorm_curd
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Phone    string `json:"phone"`
+	Address  string `json:"address"`
+	Age      int    `json:"age"`
+	Avatar   string `json:"avatar"`
+	Flag     bool   `json:"flag"`
+}
