dda74f3dabed1e0b36cb1daf118847d96b9afa3c
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-06-07 09:43:23 +0600

    file get contains any special character checker function added

--- utils/file_system.go
+++ utils/file_system.go
@@ -25,3 +25,20 @@ func IsNotExists(path string) bool {
 }
 
 
+func File_Get_Contents(filename string) ([]byte, error) {
+	fp, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+	reader := bufio.NewReader(fp)
+	contents, _ := ioutil.ReadAll(reader)
+
+	return contents, nil
+}
+
+
+
+
+
