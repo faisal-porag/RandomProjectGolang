39cf5b42c7e8b083cac5f78b1ebb15a0af4a9001
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-06-09 23:56:29 +0600

    Month Slash Year Format utility function added

--- utils/month_year_formater.go
+++ utils/month_year_formater.go
@@ -7,8 +7,20 @@ func YearDashMonthFormat(mmyy string) string {
 		strMonthYear := mmyy
 		strMonth := strMonthYear[0:2]
 		strYear := strMonthYear[len(strMonthYear)-2:]
-		strMonthDashYearFormat := "20" + strYear + "-" + strMonth //EXAMPLE : 2021-10
+		strMonthDashYearFormat := "20" + strYear + "-" + strMonth //OUTPUT EXAMPLE : 2021-10
 		return strMonthDashYearFormat
+	}
+	return "N/A"
+}
+
+
+func MonthSlashYearFormat(mmyy string) string {
+	if mmyy != "" { //INPUT EXAMPLE: 1021
+		strMonthYear := mmyy
+		strMonth := strMonthYear[0:2]
+		strYear := strMonthYear[len(strMonthYear)-2:]
+		strMonthSlashYearFormat := strMonth + "/" + strYear //OUTPUT EXAMPLE: 10/21
+		return strMonthSlashYearFormat
 	}
 	return "N/A"
 }
