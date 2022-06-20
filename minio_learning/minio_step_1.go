b7255940baebbbff28bb9258acfab1f1abb8ac07
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-06-20 10:09:48 +0600

    Initialize minio client

---
+++ minio_learning/minio_step_1.go
@@ -0,0 +1,45 @@
+package minio_learning
+
+import (
+    "log"
+
+    "github.com/minio/minio-go/v7"
+    "github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+/*
+NOTE: Some Instructions to use minio
+-------------------------------------
+Download from Github
+go get github.com/minio/minio-go/v7
+
+Initialize MinIO Client
+MinIO client requires the following four parameters specified to connect to an Amazon S3 compatible object storage.
+
+RESOURCE LINK: https://docs.min.io/docs/golang-client-quickstart-guide.html
+
+
+LOGIN LINK: https://play.min.io:9443/login
+
+USERNAME: Q3AM3UQ867SPQQA43P2F
+PASSWORD: zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG
+
+*/
+
+func InitializeMinIOClient() {
+    endpoint := "play.min.io"
+    accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+    secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+    useSSL := true
+
+    // Initialize minio client object.
+    minioClient, err := minio.New(endpoint, &minio.Options{
+        Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+        Secure: useSSL,
+    })
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    log.Printf("%#v\n", minioClient) // minioClient is now set up
+}
