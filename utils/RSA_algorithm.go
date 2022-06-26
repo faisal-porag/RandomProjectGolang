0b419388e18e5d75b76b74159c22a08c874b4430
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-06-26 09:54:38 +0600

    RSA encryption and decryption algorithm added
    RSA encryption and decryption algorithm added

---
+++ utils/RSA_algorithm.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
+var PrivateKey, keyGenerateError = rsa.GenerateKey(rand.Reader, 1024)
+
+if keyGenerateError != nil {
+  log.Println(keyGenerateError)
+}
+
+func CheckError(e error) {
+	if e != nil {
+		fmt.Println(e.Error())
+	}
+}
+
+func RSA_OAEP_Encrypt(secretMessage string, key rsa.PublicKey) string {
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rng, &key, []byte(secretMessage), label)
+	CheckError(err)
+	return base64.StdEncoding.EncodeToString(ciphertext)
+}
+
+func RSA_OAEP_Decrypt(cipherText string, privKey rsa.PrivateKey) string {
+	ct, _ := base64.StdEncoding.DecodeString(cipherText)
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), rng, &privKey, ct, label)
+	CheckError(err)
+	return string(plaintext)
+}
