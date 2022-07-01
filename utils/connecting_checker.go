f78f4e37c376e0e162a9cc20445bcc67be749c62
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-01 19:32:40 +0600

    Internet connection checker function added

---
+++ utils/connecting_checker.go
@@ -0,0 +1,16 @@
+// Internet connection checker function
+func connected() (ok bool) {
+    _, err := http.Get("http://clients3.google.com/generate_204")
+    if err != nil {
+        return false
+    }
+    return true
+}
+
+/*
+
+if !connected() {
+    fmt.Println("Check your internet connection")
+}
+
+*/
