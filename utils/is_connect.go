8d22c73304c410be728674d34fbb10efaf5c0a35
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-03 15:34:43 +0600

    internet connection checker using golang

--- utils/is_connect.go
+++ utils/is_connect.go
@@ -5,11 +5,8 @@ import (
 	"time"
 )
 
-//https://github.com/alimasyhur/is-connect/blob/master/server.go
-
 //IsOnline is a function to check wheter an internet connection
 //return bool
-
 func IsOnline() bool {
 	timeout := time.Duration(5000 * time.Millisecond)
 	client := http.Client{
@@ -17,9 +14,26 @@ func IsOnline() bool {
 	}
 	//default url to check connection is http://google.com
 	_, err := client.Get("https://google.com")
-        if err != nil {
+
+	if err != nil {
 		return false
 	}
-        return true
+
+	return true
 }
 
+//IsReachable is a function to check whether a reacheable network
+//return bool and status/error message
+func IsReachable(url string) (bool, string) {
+	timeout := time.Duration(5000 * time.Millisecond)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return false, err.Error()
+	}
+
+	return true, resp.Status
+}
