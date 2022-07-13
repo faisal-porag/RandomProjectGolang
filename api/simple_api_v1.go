5c5e85aac3cdc1024389c80ecaa6486557c99448
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-13 09:19:13 +0600

    refactor

--- api/simple_api_v1.go
+++ api/simple_api_v1.go
@@ -7,8 +7,8 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+    fmt.Fprintf(w, "Welcome to the Home Page!")
+    fmt.Println("Endpoint Hitted: homePage")
 }
 
 func handleRequests() {
