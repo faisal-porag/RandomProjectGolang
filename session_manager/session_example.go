66a7330719642528423c25ab9da50a2ef1e87385
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-18 21:54:51 +0600

    SessionId function added

--- session_manager/session_example.go
+++ session_manager/session_example.go
@@ -17,3 +17,12 @@ func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, er
     }
     return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
 }
+
+
+ func (manager *Manager) SessionId() string {
+    b := make([]byte, 32)
+    if _, err := io.ReadFull(rand.Reader, b); err != nil {
+        return ""
+    }
+    return base64.URLEncoding.EncodeToString(b)
+}
