041a6ec98d51e14c773f3fc6c5b6aa16f32f675a
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-18 21:53:53 +0600

    session manager example function added

---
+++ session_manager/session_example.go
@@ -0,0 +1,19 @@
+package session_manager
+
+
+
+
+type Manager struct {
+    cookieName  string     //private cookiename
+    lock        sync.Mutex // protects session
+    provider    Provider
+    maxlifetime int64
+}
+
+func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
+    provider, ok := provides[provideName]
+    if !ok {
+        return nil, fmt.Errorf("session: unknown provide %q (forgotten import?)", provideName)
+    }
+    return &Manager{provider: provider, cookieName: cookieName, maxlifetime: maxlifetime}, nil
+}
