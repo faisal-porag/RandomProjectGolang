82d6fff6649d14234eebec0dc24f830493eec07c
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-18 21:58:13 +0600

    session interface added

--- session_manager/session_example.go
+++ session_manager/session_example.go
@@ -10,6 +10,14 @@ type Manager struct {
     maxlifetime int64
 }
 
+ type Session interface {
+    Set(key, value interface{}) error //set session value
+    Get(key interface{}) interface{}  //get session value
+    Delete(key interface{}) error     //delete session value
+    SessionID() string                //back current sessionID
+}
+
+
 func NewManager(provideName, cookieName string, maxlifetime int64) (*Manager, error) {
     provider, ok := provides[provideName]
     if !ok {
