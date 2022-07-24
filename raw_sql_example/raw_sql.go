31bc35af0bf8a8e12bcaf1e33d38ebf106a7c5b4
MD FAISAL PORAG <porag_faisal123@hotmail.com>
2022-07-24 22:31:35 +0600

    Code refactoring

--- raw_sql_example/raw_sql.go
+++ raw_sql_example/raw_sql.go
@@ -14,24 +14,24 @@ type product struct {
    Price float64 `json:"price"`
 }
 
-func (p *product) getProduct(db *sql.DB) error {
+func (p *product) GetProduct(db *sql.DB) error {
   return db.QueryRow("SELECT name, price FROM products WHERE id=$1",
         p.ID).Scan(&p.Name, &p.Price)
 }
 
-func (p *product) updateProduct(db *sql.DB) error {
+func (p *product) UpdateProduct(db *sql.DB) error {
   _, err :=
         db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
             p.Name, p.Price, p.ID)
 return err
 }
 
-func (p *product) deleteProduct(db *sql.DB) error {
+func (p *product) DeleteProduct(db *sql.DB) error {
   _, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
 return err
 }
 
-func (p *product) createProduct(db *sql.DB) error {
+func (p *product) CreateProduct(db *sql.DB) error {
   err := db.QueryRow(
         "INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
         p.Name, p.Price).Scan(&p.ID)
@@ -41,9 +41,9 @@ if err != nil {
 return nil
 }
 
-func getProducts(db *sql.DB, start, count int) ([]product, error) {
+func GetProducts(db *sql.DB, start, count int) ([]product, error) {
   rows, err := db.Query(
-        "SELECT id, name,  price FROM products LIMIT $1 OFFSET $2",
+        "SELECT id, name, price FROM products LIMIT $1 OFFSET $2",
         count, start)
 if err != nil {
         return nil, err
